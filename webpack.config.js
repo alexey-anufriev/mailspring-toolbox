6c77404efa86367b03f65cccb21faa71a5b294af
alexey-anufriev <ice.sumy@gmail.com>
2026-01-04 00:36:38 +0100

    chore: project setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    target: "electron-renderer",
+    entry: "./src/main.ts",
+    output: {
+        path: path.resolve(__dirname, "lib"),
+        filename: "main.js",
+        library: {
+            type: "commonjs2"
+        }
+    },
+    resolve: { extensions: [".ts", ".tsx"] },
+    module: {
+        rules: [
+            {
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    externals: {
+        "mailspring-exports": "commonjs mailspring-exports",
+        "mailspring-component-kit": "commonjs mailspring-component-kit"
+    },
+    devtool: "source-map",
+};
